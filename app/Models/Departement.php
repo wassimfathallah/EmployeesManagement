34634e5977f51c51e631c5586a5300cb9b075627
wassim fathallah <wassimfathallah11@gmail.com>
2024-12-10 16:13:21 +0100

    add models

---
+++ app/Models/Departement.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Departement extends Model
+{
+    /** @use HasFactory<\Database\Factories\DepartementFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+}
---
+++ app/Models/Salaire.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Salaire extends Model
+{
+    /** @use HasFactory<\Database\Factories\SalaireFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+}
