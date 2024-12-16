c374f20cea6cdfba6ac6c31284c5a4a9344e78cf
wassim fathallah <wassimfathallah11@gmail.com>
2024-12-16 16:38:08 +0100

    integrate Sufee admin template

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
